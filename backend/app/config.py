84b809f09b10695820402bdb855ef235aeec84ad
Danila Tretiakov <d.tretiakov@innopolis.university>
2024-07-01 23:38:37 +0300

    Google auth implemented

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,7 +7,7 @@ database_user = os.getenv("POSTGRES_USER", "kuda_user")
 database_password = os.getenv("POSTGRES_PASSWORD", "123")
 database_name = os.getenv("POSTGRES_DB", 'kuda')
 database_host = os.getenv("POSTGRES_HOST", "localhost:5433")
-GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET", "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")
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET", "")
 REDIRECT_URI = os.getenv("REDIRECT_URI", 'http://localhost:8000/auth/google/callback')
 
 SQLALCHEMY_URL = f"postgresql+asyncpg://{database_user}:{database_password}@{database_host}/{database_name}"
