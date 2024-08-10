95b7f0cd7354c873a6229cdc83804f43a9d3c57e
Danila Tretiakov <daniyatretyakov@gmail.com>
2024-08-10 16:32:57 +0300

    Refactor tests

--- backend/app/tests/test_auth.py
+++ backend/app/tests/test_auth.py
@@ -1,15 +1,9 @@
 from typing import AsyncGenerator
 
 import pytest
-from fastapi.testclient import TestClient
-from fastapi import Request
-from sqlalchemy import MetaData
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncConnection, AsyncTransaction, AsyncSession
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from crud import user_crud
-from exceptions import InvalidCredentialsError, EmailInUseError, UsernameInUseError, PassNotSetException, \
-    InvalidSessionError
+from exceptions import InvalidCredentialsError, EmailInUseError, UsernameInUseError, InvalidSessionError
 from models.db_helper import test_database_helper
 from schemas.user_scheme import CredentialsScheme, CreateUserScheme
 from services import auth_service
@@ -47,13 +41,6 @@ async def test_register_user():
 
 @pytest.mark.asyncio
 async def test_login_user():
-    mock_user = CreateUserScheme(
-        email="test@mail.com",
-        username="test",
-        password="test_pass",
-        is_google_account=False
-    )
-
     mock_user_login = CredentialsScheme(
         login="test@mail.com",
         password="test_pass"
@@ -72,13 +59,6 @@ async def test_login_user():
 async def test_logout():
     async for session_obj in setup_db():
         async with session_obj as session:
-            mock_user = CreateUserScheme(
-                email="test@mail.com",
-                username="test",
-                password="test_pass",
-                is_google_account=False
-            )
-
             mock_user_login = CredentialsScheme(
                 login="test@mail.com",
                 password="test_pass"
@@ -86,10 +66,7 @@ async def test_logout():
 
             session_id = (await auth_service.login_user(mock_user_login, session))["session_id"]
 
-            mock_request = Request(scope={"type": "http", "method": "GET", "headers": {}})
-            mock_request.cookies["session_id"] = session_id
-
-            logout_message = await auth_service.logout(mock_request)
+            logout_message = await auth_service.logout(session_id)
             assert logout_message == {"message": "Logged out successfully"}
 
 
@@ -111,10 +88,7 @@ async def test_get_info():
 
             session_id = (await auth_service.login_user(mock_user_login, session))["session_id"]
 
-            mock_request = Request(scope={"type": "http", "method": "GET", "headers": {}})
-            mock_request.cookies["session_id"] = session_id
-
-            info = await auth_service.get_info(mock_request, session)
+            info = await auth_service.get_info(session_id, session)
 
             assert info.username == mock_user.username
             assert info.email == mock_user.email
@@ -124,12 +98,5 @@ async def test_get_info():
 async def test_invalid_session():
     async for session_obj in setup_db():
         async with session_obj as session:
-            mock_request = Request(scope={"type": "http", "method": "GET", "headers": {}})
-            mock_request.cookies["session_id"] = "invalid_session_id"
-
             with pytest.raises(InvalidSessionError):
-                await auth_service.get_session_id(mock_request)
-
-            with pytest.raises(InvalidSessionError):
-                mock_request = Request(scope={"type": "http", "method": "GET", "headers": {}})
-                await auth_service.get_user_from_session_id(mock_request, session)
+                await auth_service.get_user_from_session_id("invalid_session_id", session)
--- backend/app/tests/test_google_auth.py
+++ backend/app/tests/test_google_auth.py
@@ -1,11 +1,10 @@
 import pytest
-from fastapi import Request
 
 from crud import user_crud
-from exceptions import PassNotSetException, InvalidCredentialsError, UsernameInUseError
+from exceptions import PassNotSetException, InvalidCredentialsError
+from schemas import user_scheme
 from schemas.user_scheme import CredentialsScheme
 from services import auth_service
-from schemas import user_scheme
 from .test_auth import setup_db
 
 mock_user = user_scheme.CreateUserScheme(
@@ -25,7 +24,7 @@ async def test_google_register_user_without_pass():
     async for session_obj in setup_db():
         async with session_obj as session:
             user = await user_crud.create(mock_user, session)
-            session_id = await auth_service.create_session(user.user_id, user.username)
+            await auth_service.create_session(user.user_id, user.username)
 
             with pytest.raises(PassNotSetException):
                 await auth_service.login_user(mock_login, session)
@@ -40,10 +39,7 @@ async def test_google_set_user_pass():
 
             session_id = await auth_service.create_session(user.user_id, user.username)
 
-            mock_request = Request(scope={"type": "http", "method": "GET", "headers": {}})
-            mock_request.cookies["session_id"] = session_id
-
-            result = await auth_service.set_pass("new_pass", mock_request, session)
+            result = await auth_service.set_pass("new_pass", session_id, session)
             assert result == {'message': 'Password successfully set'}
             await session.close()
 
@@ -56,10 +52,7 @@ async def test_google_login_with_new_pass():
 
             session_id = await auth_service.create_session(user.user_id, user.username)
 
-            mock_request = Request(scope={"type": "http", "method": "GET", "headers": {}})
-            mock_request.cookies["session_id"] = session_id
-
-            await auth_service.set_pass("new_pass", mock_request, session)
+            await auth_service.set_pass("new_pass", session_id, session)
 
             mock_login.password = "new_pass"
 
@@ -76,10 +69,7 @@ async def test_google_login_with_wrong_pass():
 
             session_id = await auth_service.create_session(user.user_id, user.username)
 
-            mock_request = Request(scope={"type": "http", "method": "GET", "headers": {}})
-            mock_request.cookies["session_id"] = session_id
-
-            await auth_service.set_pass("new_pass", mock_request, session)
+            await auth_service.set_pass("new_pass", session_id, session)
 
             mock_login.password = "incorrect_password"
             with pytest.raises(InvalidCredentialsError):
